9945476a3757c168bbc20ef9d3aca2ea27f190a0
Sofia <katzmansof@gmail.com>
2025-04-06 15:20:11 -0400

    add about page routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + "/public/"));
@@ -6,6 +7,11 @@ app.use(express.static(__dirname + "/public/"));
 app.get("/", function (req, resp){
     resp.sendFile("index.html");
 })
+
+app.get("/about", function (req, resp) {
+    resp.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
 
 app.listen(3000, function(){
     console.log("listening on port: 3000")
