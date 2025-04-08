faad2034c79c01d3b96f636fe0174da3d1d0c6ed
Sofia Katzman <sofiakatzman@Sofias-MacBook-Pro.local>
2025-04-07 23:02:45 -0400

    remove op when hit

--- public/javascripts/models/opponent.js
+++ public/javascripts/models/opponent.js
@@ -11,9 +11,29 @@ class Opponent{
     }
 
     update(){
+        const lasers = Laserbeam.all
+        const opHit = lasers.find(laser =>   
+            this.x < laser.x + laser.width &&
+            this.x + this.width > laser.x &&
+            this.y < laser.y + laser.height &&
+            this.y + this.height > laser.y
+        )
+        
+        // remove opponent
+        if(opHit){
+            console.log("op defeated!");
+            const index = lasers.indexOf(opHit);
+            if(index > -1){
+                lasers.splice(index, 1);
+            }
 
+            const opIndex = Opponent.all.indexOf(this);
+            if(opIndex) console.log(opIndex);
+            if(opIndex > -1){
+                Opponent.all.splice(opIndex, 1);
+            }
+        }
     }
-
     draw(){
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
